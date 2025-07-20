d16bbddc7f66d53de0ec832df793636d7d81951d
giorgio.arcara <giorgio.arcara@gmail.com>
2025-07-20 17:16:15 +0200

    comparison with Aiello code

--- Tutorial/DEMO_analysis_GEMS.R
+++ Tutorial/DEMO_analysis_GEMS.R
@@ -39,4 +39,61 @@ Test.ES = ES(adjscores=Test.ARC.res$new.df$ADJ_SCORES)
 
 print(Test.ES)
 
+dim(Test.dat)
+
+
+# the following code include a comparison with code from Aiello as it is.
+# NOTE: that the functions I used are based on Aiello, so this check only exclude some transcriptions issues.
+
+tolLimits.obs(n=644)
+
+
+#insert the sample size and the oTL to get the last Equivalent Scores (ESs); rounding controls are also provided
+n <- 644
+oTL <- 23
+cd1 <- oTL/n
+z1 <- qnorm(cd1)
+
+z1_3 <- z1/3
+z1_2 <- z1_3*2
+
+cd2 <- pnorm(z1_2)
+a <- (cd1-cd2)*n
+a_r <- round(a)
+ES1 <- -a_r+oTL
+ES1
+
+a
+a_r
+
+cd3 <- pnorm(z1_3)
+b <- (cd3-cd2)*n
+b_r <- round(b)
+ES2 <- ES1+b_r
+ES2
+
+b
+b_r
+
+cd4 <- pnorm(0)
+c <- (cd4-cd3)*n
+c_r <- round(c)
+ES3 <- ES2+c_r
+ES3
+
+c
+c_r
+
+
+Test.ARC.res$new.df$ADJ_SCORES[23]
+
+### Check with different code
+oTL == Test.ES$Observations[1]
+ES1 == Test.ES$Observations[2]
+ES2 == Test.ES$Observations[3]
+ES3 == Test.ES$Observations[4]
+
+
+
+
 
